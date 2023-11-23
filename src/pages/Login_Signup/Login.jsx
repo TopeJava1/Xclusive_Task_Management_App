dff978c753757b0dfcf6a968785bb76ca1db5c2b
Topejava1 <topejava@gmail.com>
2023-11-23 17:06:35 +0100

    creating login page2

--- src/pages/Login_Signup/Login.jsx
+++ src/pages/Login_Signup/Login.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './index.css'
+import { Form, Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div className='logman'>
         <div className='logman-box'>
+            <h1>Lgin</h1>
+            <form action="">
+                <input type='text' placeholder='Enter your Username or Email'/>
+                <input type='password' placeholder='Enter your Password'/>
+                <button></button>
 
+                </form>
+                <p> Don't have an account yet? <Link> Signup</Link> </p>
+g
         </div>
     </div>
   )
