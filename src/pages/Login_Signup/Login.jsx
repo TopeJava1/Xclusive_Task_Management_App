a834057261897158083dc8d27ef7fd212eebf1a7
Topejava1 <topejava@gmail.com>
2023-11-23 17:04:33 +0100

    creating login page

---
+++ src/pages/Login_Signup/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const Login = () => {
+  return (
+    <div className='logman'>
+        <div className='logman-box'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
